f08ff361211ca58579d6ef8a99353d0a412c1a6e
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-18 16:12:03 +0530

    Create lar.c

---
+++ apl/lar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int a,b,c;
+int largest;
+printf("enter three numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && d>c)
+largest=a;
+else if(b>a && b>c)
+largest=b;
+else
+largest=c;
+printf("largest number is=%d",largest);
+return 0;
+}
