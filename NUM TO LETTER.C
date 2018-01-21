be9b7a5013f7ba18cf1bdf760418e6a2d768ebc6
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-21 16:40:58 +0530

    Create NUM TO LETTER.C

---
+++ NUM TO LETTER.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+int g;
+scanf("%d",&g);
+switch(g)
+{
+case:0
+printf("zero");
+break;
+case:1
+printf("one");
+break;
+case:2
+printf("two");
+break;
+case:3
+printf("three");
+break;
+case:4
+printf("four");
+break;
+case:5
+printf("five");
+break;
+case:6
+printf("six");
+break;
+case:7
+printf("eight");
+break;
+}
+return 0;
+}
+
