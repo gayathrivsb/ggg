b0c20e318fd4304daf667ad51fb3311a4cc25869
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-23 16:56:26 +0530

    Create count spaces.c

---
+++ count spaces.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,space;
+char a[50];
+scanf("%s",&a);
+for(i=o;a[i]<=0;i++)
+{
+if(a[i]==' ');
+space++;
+}
+printf("the number of space is:%d",space);
+return 0;
+}
