878cc494cd3a9bfe67b2aebeb507301946129cd7
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-23 16:31:58 +0530

    Create prime bet 2.c

---
+++ prime bet 2.c
@@ -0,0 +1,14 @@
+#include<stdio.>
+int main()
+{
+int num,a,b,c;
+printf("enter the range:");
+scanf("%d %d",&a,&b):
+for(num=a;num<=b;num++)
+{
+if(num%c==0)
+break;
+}
+if(c==num)
+printf("%d",num)
+}
