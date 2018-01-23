5a5255df9dbf77f69ebb830d5a8d96bbfbeda58d
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-23 16:00:14 +0530

    Create char31.c

---
+++ char31.c
@@ -0,0 +1,24 @@
+
+
+#include<iostream>
+#include<string.h>
+int main()
+{
+	char str[40];
+	cout << "enter string :";
+	cin>>str[i];
+	int words=0;
+	for( int i=0;str[i]!=0;i++)
+	{
+		if(str[i]==' ')
+		{
+			words++;
+			
+		}
+		cout <<"the number of words="<<words+1;
+		
+	}
+	return 0;
+	
+}
+
