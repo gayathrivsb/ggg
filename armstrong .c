54c441aa6f0c255669d465ca6719c444ed58c852
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-23 16:46:45 +0530

    Create armstrong .c

---
+++ armstrong .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,num,d,temp,sum=0;
+scanf("%d",&a);
+while(num>=1)
+{
+num=d;
+d=(num%10);
+sum=sum=d*d*d;
+num=num/10;
+}
+printf("armstrong");
+return 0;
+}
