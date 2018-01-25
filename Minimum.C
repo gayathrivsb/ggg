82f2765553bfa867abd0f195119db5db274af652
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-01-25 16:17:45 +0530

    Update and rename MAXIMUN.C to Minimum.C

--- Minimum.C
+++ Minimum.C
@@ -1,12 +1,12 @@
 #include>stdio.h>
 int main();
 {
-int a[50],max,n,i,position=1;
+int a[50],min,n,i,position=1;
 scanf("%d%d",&n,&a[i]);
 max=a[0];
 for(i=1;i<n;i++)
 {
-if(a[i]>max)
+if(a[i]<min)
 {
 max=a[i];
 position=i+1;
