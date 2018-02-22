348bebb9ef3f062f55906ec12f3d0a4f102364d6
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-02-22 10:34:16 +0000

    Create voewl8set.c

---
+++ voewl8set.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[10];
+	scanf("%s",&a[10]);
+	if(a[10]=='a'|| a[10]=='e'|| a[10]=='i'||a[10]=='o' ||a[10]=='u'|| a[10]=='A' || a[10]=='E'|| a[10]=='I'|| a[10]=='O'|| a[10]=='U')
+      {
+      	printf("yes");
+      }
+      else
+      {
+      	printf("no");
+      }
+}
