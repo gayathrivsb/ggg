29fd6ffe588c329222e4e99fc7b98c93b569eb4c
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-02-22 11:17:14 +0000

    Create 91 cuboid.c

---
+++ 91 cuboid.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int leng,wid,height,a;
+	scanf("%d%d%d",&leng,&wid,&height);
+	a=leng*wid;
+	printf("surface area is:%d",a);
+	printf("volume is:%d",a*a*a);
+	
+}
