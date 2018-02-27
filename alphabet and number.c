7519e022bfb3431c174b9be8a965fbdf5cedb928
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-02-27 11:23:51 +0000

    Create alphabet and number.c

---
+++ alphabet and number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char a[10];
+	int i;
+	scanf("%c",a[i]);
+	if((a[i]>='a')||(a[i]<='z')||(a[10]>0||a[10]<=9))
+	{
+		printf("both alphabets");
+	}
+else
+{
+	printf("no");
+}
+return 0;
+}
